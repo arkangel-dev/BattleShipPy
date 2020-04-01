969696dbfbeed4dd78e132099c2869e65de505cc
Sam Ramirez <asdurftech@gmail.com>
2020-04-01 13:08:34 +0500

    Updated stuff

--- JavaClient/JaBattleShip/src/battleship/App.java
+++ JavaClient/JaBattleShip/src/battleship/App.java
@@ -2,29 +2,12 @@ package battleship;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-		JaBattleship Server = new JaBattleship(
-			"Ark",
-			"127.0.0.1",
-			 9999
-		); // Define the connection
-
-		// while (true) {
-		// 	Server.UpdateTelemetry(); // Update the telemetry
-		// 	Server.Move(1, 1); // Move the ship
-		// 	Server.Commit(); // Commit to the appended commands
-		// 	Thread.sleep(250); // Sleep
-
-		// 	System.out.println(
-		// 		"X : " + Server.Me.x +
-		// 		" | y " + Server.Me.y
-		// 	); // Print the current position
-		// }
+		JaBattleship Client = new JaBattleship("Ark", "127.0.0.1", 9999);
 		while (true) {
-			Server.UpdateTelemetry();
-			Thread.sleep(1000);
-			Server.SelfDestruct();
-			Server.Commit();
+			Client.UpdateTelemetry();
+			// Insert battle logic here...
+			Client.Commit();
+			Thread.sleep(250);
 		}
     }
 }
